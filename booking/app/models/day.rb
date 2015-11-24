a061a7440a1a9a22cc5ae906eba5e338e36441d3
Will <williamrwilson@gmail.com>
2015-11-24 09:29:54 -0500

    add totals to visits

--- booking/app/models/day.rb
+++ booking/app/models/day.rb
@@ -1,5 +1,4 @@
 class Day < ActiveRecord::Base
   belongs_to :visit
-
-
+  validates_numericality_of :breakfast, :lunch, :dinner, :hh, :dorm, :lodge
 end
--- booking/app/models/visit.rb
+++ booking/app/models/visit.rb
@@ -10,4 +10,52 @@ class Visit < ActiveRecord::Base
     number_days = (end_date - start_date).to_i + 1
   end
 
+  def total_breakfast
+    total = 0
+    self.days.each do |day|
+      total += day.breakfast
+    end
+    total
+  end
+
+  def total_lunch
+    total = 0
+    self.days.each do |day|
+      total += day.lunch
+    end
+    total
+  end
+
+  def total_dinner
+    total = 0
+    self.days.each do |day|
+      total += day.dinner
+    end
+    total
+  end
+
+  def total_dorm
+    total = 0
+    self.days.each do |day|
+      total += day.dorm
+    end
+    total
+  end
+
+  def total_hh
+    total = 0
+    self.days.each do |day|
+      total += day.hh
+    end
+    total
+  end
+
+  def total_lodge
+    total = 0
+    self.days.each do |day|
+      total += day.lodge
+    end
+    total
+  end
+  
 end
