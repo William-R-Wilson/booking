8957c32cff5504e3737bfbe936f278e9c76aa1ff
Will <williamrwilson@gmail.com>
2015-11-23 21:38:41 -0500

    add day/edit and table view of days for visit/show

--- booking/app/controllers/days_controller.rb
+++ booking/app/controllers/days_controller.rb
@@ -2,7 +2,22 @@ class DaysController < ApplicationController
 
   def edit
     @day = Day.find(params[:id])
+    @visit = Visit.where("visit_id = ?", @day.visit_id)
+    #@guest = Guest.where("id = ?", @visit.guest_id)
   end
+
+  def update
+    @day = Day.find(params[:id])
+    visit = @day.visit_id
+    respond_to do |format|
+      if @day.update(day_params)
+        format.html { redirect_to visit_path(visit), notice: "Day was successfully updated" }
+      else
+        format.html { render :edit }
+      end
+    end
+  end
+
 
   def index
 
@@ -11,7 +26,7 @@ class DaysController < ApplicationController
   private
 
     def day_params
-      params.require(:day).permit(:visit_id, :breakfast,
+      params.require(:day).permit(:id, :visit_id, :breakfast,
                   :lunch, :dinner, :dorm, :hh, :lodge, :date)
     end
 
