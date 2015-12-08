158402c25c6682d293ad4284a0da8c8756f79638
Will <williamrwilson@gmail.com>
2015-12-08 11:12:21 -0500

    change from sqlite to pg

--- booking/db/schema.rb
+++ booking/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20151125144037) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "days", force: :cascade do |t|
     t.integer  "breakfast"
     t.integer  "lunch"
