2c3bd0e903bf2398d3120977f0de5df252af8303
Will <williamrwilson@gmail.com>
2016-01-05 07:26:17 -0500

    basic calendar functionality

--- booking/app/controllers/calendar_controller.rb
+++ booking/app/controllers/calendar_controller.rb
@@ -2,6 +2,8 @@ class CalendarController < ApplicationController
 
   def show
     @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @visits = Visit.where start_date: (@date.beginning_of_month)..(@date.end_of_month)
+    @visit_dates = @visits.group_by(&:start_date)
   end
 
 end
