7e3032a46f3a10fb55994071adaa30f5ed0add3c
Will <williamrwilson@gmail.com>
2016-01-22 13:00:15 -0500

    changed employee views to haml

---
+++ booking/spec/features/costing_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe "Costing" do
+  before(:all) do
+    @price = Price.create(breakfast: 10,
+                          lunch: 12,
+                          dinner: 14,
+                          dorm: 45,
+                          horton: 60,
+                          lodge: 60,
+                          rentalfee: 150
+                          )
+    @guest = Guest.create(email: "group@group.com",
+                          address: "123 12th St.",
+                          city: "Knoxville",
+                          state: "TN",
+                          name: "Group",
+                          bill_to: "Group",
+                          )
+  end
+  before(:each) do
+    @visit1 = Visit.create(num_attendees: 20,
+                        start_date: "2016-01-01",
+                        end_date: "2016-01-02",
+                        guest_id: @guest.id,
+                        needs_projector: true,
+                        needs_childcare: true,
+                        needs_stafftime: true,
+                        needs_supplies: true,
+                        status: 0,
+                        price_id: @price.id
+                        )
+    end
+
+  describe "Show" do
+    it "shows basic cost info" do
+      visit costing_path(@visit1)
+      within "h3" do
+        expect(page).to have_content "Group"
+      end
+    end
+  end
+
+end
--- booking/spec/models/day_spec.rb
+++ booking/spec/models/day_spec.rb
@@ -8,8 +8,8 @@ describe Day, :type => :model do
   it "has a date" do
     expect(@day.date).to be_a(Date)
   end
-  it "initializes with a default hours value of 24" do
-    expect(@day.hours).to eq(24)
+  it "initializes with a default hours value of 0" do
+    expect(@day.hours).to eq(0)
   end
   it "initializes with a default waive_facility_rental set to false" do
     expect(@day.waive_facility_rental).to eq(false)
